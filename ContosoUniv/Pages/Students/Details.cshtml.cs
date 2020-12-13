69416c4dc78c0315c6b7d0b54dfb8676e89fd509
mhm <mhmhatim@gmail.com>
2020-12-13 18:20:01 +0100

    ContosoUniversity

---
+++ ContosoUniv/Pages/Students/Details.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ContosoUniv.Data;
+using ContosoUniv.Models;
+
+namespace ContosoUniv.Pages.Students
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly ContosoUniv.Data.SchoolContext _context;
+
+        public DetailsModel(ContosoUniv.Data.SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public Student Student { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Student = await _context.Students.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (Student == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
